13f4b6d744f85c6d10183a03239352ae80f5e377
Krishnadas <skrishnadas38@gmail.com>
2024-12-12 17:54:37 +0530

    final1

--- event-hive-backend/src/infrastructure/config/db.ts
+++ event-hive-backend/src/infrastructure/config/db.ts
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
-const DB_STRING: string = process.env.DB_STRING || 'mongodb://127.0.0.1:27017/event-hive'
+const DB_STRING: string = process.env.DB_STRING || 'mongodb://127.0.0.1:27017/event-hive';
 
 const connectDb = async () => {
     try {
-        await mongoose.connect(DB_STRING);
+        await mongoose.connect(DB_STRING, {
+            serverSelectionTimeoutMS: 30000, // Wait for 30 seconds before timing out
+            socketTimeoutMS: 45000,          // Allow 45 seconds for socket connection
+        });
         
         console.log(`Database connected on ${mongoose.connection.host}`);
     } catch (error) {
